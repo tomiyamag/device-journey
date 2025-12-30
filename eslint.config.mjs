5c993b504c7cfb2201895f3e6c3176bb43c39558
tomiyamag <tomiyama@nadia.bz>
2025-12-30 16:18:55 +0900

    update: eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,23 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import eslintConfigPrettier from "eslint-config-prettier/flat";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
   // Override default ignores of eslint-config-next.
+  {
+    plugins: {
+      "simple-import-sort": simpleImportSort,
+    },
+    rules: {
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
+    },
+  },
+  eslintConfigPrettier,
   globalIgnores([
     // Default ignores of eslint-config-next:
     ".next/**",
