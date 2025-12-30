70a4bf4a00f0978c5ea9e6c8a4282956f77e145d
tomiyamag <tomiyama@nadia.bz>
2025-12-31 00:20:12 +0900

    update: サインアップ、サインアウト実装

--- proxy.ts
+++ proxy.ts
@@ -3,9 +3,9 @@ import { type NextRequest } from "next/server";
 import { updateSession } from "@/lib/supabase/proxy";
 
 export async function proxy(request: NextRequest) {
-  // update user's auth session
   return await updateSession(request);
 }
+
 export const config = {
   matcher: [
     /*
