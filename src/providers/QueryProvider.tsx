f0d813d3d2f53f22bff59568c09111c2c7e452ed
tomiyamag <tomiyama@nadia.bz>
2026-01-04 19:45:28 +0900

    update: query provider 描画ごとに new されないよう調整

--- src/providers/QueryProvider.tsx
+++ src/providers/QueryProvider.tsx
@@ -1,11 +1,11 @@
 "use client";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactNode } from "react";
-
-const queryClient = new QueryClient();
+import { ReactNode, useState } from "react";
 
 export default function QueryProvider({ children }: { children: ReactNode }) {
+  const [queryClient] = useState(() => new QueryClient());
+
   return (
     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
   );
