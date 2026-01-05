6f7e6987815c6e965121784faaf93a4477a3f2f9
tomiyamag <tomiyama@nadia.bz>
2026-01-05 20:14:34 +0900

    update: stylelint 追加

---
+++ stylelint.config.mjs
@@ -0,0 +1,10 @@
+/** @type {import('stylelint').Config} */
+const config = {
+  extends: [
+    "stylelint-config-recommended",
+    "stylelint-config-tailwindcss",
+    "stylelint-config-recess-order",
+  ],
+};
+
+export default config;
