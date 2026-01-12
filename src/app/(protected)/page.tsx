f2c5c2fe784ecfce93b9f6e3c851cb938cd5ac28
tomiyamag <tomiyama@nadia.bz>
2026-01-12 23:17:29 +0900

    fix: 本番環境でのみ Hydration エラー発生のため、動作確認用にコンポーネントをコメントアウト 3

--- src/app/(protected)/page.tsx
+++ src/app/(protected)/page.tsx
@@ -4,17 +4,18 @@ import OtherDevices from "@/components/organisms/OtherDevices";
 
 export default function HomePage() {
   return (
-    <div className="flex flex-col gap-9">
-      <section>
-        <div className="flex flex-col gap-2">
-          {/* <ActiveDevice /> */}
-          <Pickups />
-        </div>
-      </section>
+    // <div className="flex flex-col gap-9">
+    //   <section>
+    //     <div className="flex flex-col gap-2">
+    //       <ActiveDevice />
+    //       <Pickups />
+    //     </div>
+    //   </section>
 
-      <section>
-        <OtherDevices />
-      </section>
-    </div>
+    //   <section>
+    //     <OtherDevices />
+    //   </section>
+    // </div>
+    <div>トップページ</div>
   );
 }
